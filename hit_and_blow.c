2f4a331ce2a8ac776316c17e29a4e1313f1abfc5
Shota <tennten266@gmail.com>
2026-07-03 17:59:00 +0900

    Add random number generation and game logic

--- hit_and_blow.c
+++ hit_and_blow.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 
 int f_pow(int s, int n) {
@@ -15,6 +16,7 @@ int f_pow(int s, int n) {
 int n = 4;
 
 int main(void) {
+    srand(time(NULL));
     
     int a[n];
     do {
@@ -24,40 +26,66 @@ int main(void) {
         a[i] = rand() % 10;
     }
 
-    int ansbox[10];
-    memset(ansbox, 0, sizeof(ansbox));
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < n; j++) {
-            if(i == a[j]) {
-                ansbox[i]++;
-            }
-        }
+    int number = 0;
+    for (int i = 0; i < n; i++)  {
+        number += a[i] * f_pow(10, n-1-i);
     }
 
 
     int response, hit_count, blow_count;
-    printf("桁数は%d\n", n);
+    printf("桁数は%dです\n", n);
+    int ans_count = 1;
 
     do {
         hit_count = 0;
         blow_count = 0;
 
+        printf("%d回目の挑戦です\n", ans_count);
         printf("what do you think number is ?\n");
         if(scanf("%d", &response) != 1) {
-            printf("数字を入力してください\n");
-            getchar()
+            printf("数字を入力してください\n\n");
+            getchar();
             continue;
-        }
-
+        } 
+            char str[256];
+            snprintf(str, sizeof(str), "%d", response);
+            if(strlen(str) != n) {
+                if(response == 0) {
+                    printf("%d\n", number);
+                    printf("you lose\n");
+                    return 1;
+                }
+                printf("数字は%d桁です\n\n", n);
+                getchar();
+                continue; 
+            }
+            
         
 
+        //hit_count
+        for (int i = 0; i < n; i++) {
+            if(str[i] == a[i] + '0') {
+                hit_count++;
+            }
+        }
+        
+        //blow_count
+        for (int i = 0; i < n; i++) {       // 入力された文字のループ
+            for (int j = 0; j < n; j++) {   // 正解配列のループ
+                if(i != j && str[i] == a[j] + '0') {
+                    blow_count++;
+                }
+            }
+        }
 
         printf("hit_count = %d\n", hit_count);
-        printf("blow_count = %d\n", blow_count);
+        printf("blow_count = %d\n\n", blow_count);
+        ans_count++;
+        
 
     } while(hit_count != n);
+    printf("Congraturation!!!!\n");
+    printf("%d回目で成功しました", ans_count-1);
 
     return 0;
-
 }
