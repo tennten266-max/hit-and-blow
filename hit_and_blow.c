2d8678355cd50da6f6139286336eba2ef3971a05
Shota <tennten266@gmail.com>
2026-07-04 09:02:30 +0900

    hit_and_blow.c を更新
    breakをついか

--- hit_and_blow.c
+++ hit_and_blow.c
@@ -73,6 +73,7 @@ int main(void) {
         for (int i = 0; i < n; i++) {       // 入力された文字のループ
             for (int j = 0; j < n; j++) {   // 正解配列のループ
                 if(i != j && str[i] == a[j] + '0') {
+                    break;
                     blow_count++;
                 }
             }
